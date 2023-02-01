aa727d837316a02682239182129c3f8a8e796296
Robert Hardy <rhardy1@dcsdk12.org>
2023-02-01 16:25:03 -0700

    Added projectiles

--- scripts/app.js
+++ scripts/app.js
@@ -8,15 +8,55 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
 
+class Projectile {
+	constructor(sx, sy, tx, ty) {
+		this.dx = sx - tx;
+		this.dy = sy - ty;
+		this.tx = tx;
+		this.ty = ty;
+
+		this.x = sx;
+		this.y = sy;
+		this.isVisible = true;
+		this.speed = 20;
+	}
+
+	update(elapsedTime) {
+		if (this.x == this.tx && this.y == this.ty) {
+			this.isVisible = false;
+			return;
+		}
+
+		this.x -= this.dx / this.speed;
+		this.y -= this.dy / this.speed;
+	}
+
+	draw() {
+		if (!this.isVisible) return;
+
+		ctx.save();
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, 3, 0, Math.PI * 2);
+		ctx.fillStyle = "red";
+		ctx.fill();
+		ctx.restore();
+	}
+}
+
 class Turret {
-	constructor() {
-		this.x = canvas.width / 2;
-		this.y = canvas.height / 2;
+	constructor(sx, sy) {
+		this.x = sx;
+		this.y = sy;
 		this.color = "black";
 		this.angle = 0;
 
 		/** @type { RoundThing | null } */
 		this.target = null;
+
+		/** @type { Projectile[] } */
+		this.projectiles = [];
+		this.rateOfFire = 250; // milliseconds
+		this.lastFireTime = 0;
 	}
 
 	setTarget(target) {
@@ -26,12 +66,29 @@ class Turret {
 	update(elapsedTime) {
 		if (this.target == null) return;
 
+		this.lastFireTime += elapsedTime;
+
+		if (this.lastFireTime >= this.rateOfFire) {
+			this.lastFireTime = 0;
+			this.projectiles.push(
+				new Projectile(this.x, this.y, this.target.x, this.target.y)
+			);
+		}
+
+		this.projectiles.forEach((p) => {
+			p.update();
+		});
+
 		let dx = this.x - this.target.x;
 		let dy = this.y - this.target.y;
 		this.angle = Math.atan2(dy, dx);
 	}
 
 	draw() {
+		this.projectiles.forEach((p) => {
+			p.draw();
+		});
+
 		ctx.save();
 
 		ctx.translate(this.x, this.y);
@@ -105,8 +162,19 @@ for (let i = 0; i < 1; i++) {
 	roundThings.push(new RoundThing());
 }
 
-let t1 = new Turret();
-t1.setTarget(roundThings[0]);
+let turrets = [
+	new Turret(100, 100),
+	new Turret(canvas.width - 100, 100),
+	new Turret(100, canvas.height - 100),
+	new Turret(canvas.width - 100, canvas.height - 100),
+];
+
+turrets.forEach((t) => {
+	t.setTarget(roundThings[0]);
+});
+
+// let t1 = new Turret(100, 100);
+// t1.setTarget(roundThings[0]);
 
 let lastDirectionChange = 0;
 let currentTime = 0;
@@ -124,8 +192,13 @@ function gameLoop(timestamp) {
 		r.draw();
 	});
 
-	t1.update(elapsedTime);
-	t1.draw();
+	turrets.forEach((r) => {
+		r.update(elapsedTime);
+		r.draw();
+	});
+
+	// t1.update(elapsedTime);
+	// t1.draw();
 
 	window.requestAnimationFrame(gameLoop);
 }
