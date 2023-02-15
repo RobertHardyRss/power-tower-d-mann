a9aebd2ceececccc03795fa93e73528fc5bfee12
zach <Frenchzj@s.dcsdk12.org>
2023-02-15 15:48:20 -0700

    initial command

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,94 @@
 //@ts-check
+import { Enemy } from "./game-objects/enemy.js";
+import { Projectile } from "./game-objects/projectile.js";
+import { Turret } from "./game-objects/turret.js";
+
 /** @type {HTMLCanvasElement} */
 //@ts-ignore canvas is an HTMLCanvasElement
 const canvas = document.getElementById("game-canvas");
+/** @type {CanvasRenderingContext2D} */
+//@ts-ignore
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
+
+class Game {
+	constructor() {
+		this.player = {
+			health: 100,
+			credits: 0,
+		};
+		this.gridSize = 32;
+		this.currentLevel = undefined;
+
+		/** @type { Turret[] } */
+		this.turrets = [];
+		/** @type { Enemy[] } */
+		this.enemies = [];
+		/** @type { Projectile[] } */
+		this.projectiles = [];
+	}
+
+	initTurrets() {
+		this.turrets.forEach((t) => {
+			t.targets = this.enemies;
+		});
+	}
+
+	update() {
+		this.projectiles = [];
+		this.turrets.forEach((t) => {
+			t.targets = this.enemies;
+			this.projectiles.concat(t.projectiles);
+		});
+
+	}
+}
+
+class GameLevel {
+	constructor() {}
+}
+
+let game = new Game();
+
+game.turrets = [
+	new Turret(ctx, 100, 100),
+	new Turret(ctx, canvas.width - 100, 100),
+	new Turret(ctx, 100, canvas.height - 100),
+	new Turret(ctx, canvas.width - 100, canvas.height - 100),
+];
+
+for (let i = 0; i < 1; i++) {
+	game.enemies.push(new Enemy(ctx, canvas.width / 2, canvas.height / 2));
+}
+
+game.initTurrets();
+
+let lastDirectionChange = 0;
+let currentTime = 0;
+
+function gameLoop(timestamp) {
+	let elapsedTime = timestamp - currentTime;
+	currentTime = timestamp;
+	lastDirectionChange += elapsedTime;
+
+	console.log(timestamp, elapsedTime);
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	game.enemies.forEach((r) => {
+		r.update(elapsedTime);
+		r.draw();
+	});
+
+	game.turrets.forEach((r) => {
+		r.update(elapsedTime);
+		r.draw();
+	});
+
+	// t1.update(elapsedTime);
+	// t1.draw();
+
+	window.requestAnimationFrame(gameLoop);
+}
+
+window.requestAnimationFrame(gameLoop);
---
+++ scripts/game-objects/turret.js
@@ -0,0 +1,121 @@
+//@ts-check
+
+import { Projectile } from "./projectile.js";
+import { Enemy } from "./enemy.js";
+
+export class Turret {
+	/**
+	 * @param {CanvasRenderingContext2D} ctx
+	 * @param {number} sx
+	 * @param {number} sy
+	 */
+	constructor(ctx, sx, sy) {
+		this.ctx = ctx;
+		this.x = sx;
+		this.y = sy;
+		this.color = "black";
+		this.angle = 0;
+
+		this.range = 300;
+
+		/** @type {Enemy[]} */
+		this.targets = [];
+		/** @type { Enemy | null } */
+		this.target = null;
+
+		/** @type { Projectile[] } */
+		this.projectiles = [];
+		this.rateOfFire = 250; // milliseconds
+		this.lastFireTime = 0;
+	}
+
+	acquireTarget() {
+		// if my current target is out of range, set target to null
+		// if I already have a target return
+		if (this.target != null) {
+			if (this.getDistanceToTarget(this.target) > this.range) {
+				this.target = null;
+				// this.projectiles = [];
+				return;
+			}
+		}
+
+		// if targets are within my range, target the closest
+		this.targets.forEach((t) => {
+			if (this.getDistanceToTarget(t) <= this.range) {
+				this.target = t;
+			}
+		});
+	}
+
+	getDistanceToTarget(target) {
+		// if we don't have a target make the distance as big
+		// as possible (WAY out of range)
+		if (target == null) return Number.MAX_VALUE;
+
+		let dx = this.x - target.x;
+		let dy = this.y - target.y;
+		let distance = Math.hypot(dx, dy);
+		return distance;
+	}
+
+	update(elapsedTime) {
+		this.acquireTarget();
+
+		this.projectiles.forEach((p) => {
+			p.update();
+		});
+
+		// filter out projectiles that are no longer visible
+		this.projectiles = this.projectiles.filter((p) => p.isVisible);
+
+		if (this.target == null) return;
+
+		this.lastFireTime += elapsedTime;
+
+		if (this.lastFireTime >= this.rateOfFire) {
+			this.lastFireTime = 0;
+			this.projectiles.push(
+				new Projectile(
+					this.ctx,
+					this.x,
+					this.y,
+					this.target.x,
+					this.target.y
+				)
+			);
+		}
+
+		let dx = this.x - this.target.x;
+		let dy = this.y - this.target.y;
+		this.angle = Math.atan2(dy, dx);
+	}
+
+	draw() {
+		this.projectiles.forEach((p) => {
+			p.draw();
+		});
+
+		this.ctx.save();
+		this.ctx.beginPath();
+		this.ctx.arc(this.x, this.y, this.range, 0, Math.PI * 2);
+		this.ctx.fillStyle = "hsla(0, 100%, 50%, 0.5)";
+		this.ctx.fill();
+		this.ctx.restore();
+
+		this.ctx.save();
+		this.ctx.translate(this.x, this.y);
+		this.ctx.rotate(this.angle);
+
+		this.ctx.beginPath();
+		this.ctx.moveTo(-10, 0);
+		this.ctx.lineTo(10, -5);
+		this.ctx.lineTo(10, 5);
+		this.ctx.lineTo(-10, 0);
+
+		this.ctx.fillStyle = this.color;
+		this.ctx.fill();
+
+		this.ctx.restore();
+	}
+}
